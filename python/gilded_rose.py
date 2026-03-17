225bbd10cc79db106de5f5f5348c48b53137cfb9
NandiniBommireddy <bnandinireddy0181@gmail.com>
2026-03-16 19:28:17 -0700

    Refactor Gilded Rose with item updaters

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
@@ -10,38 +11,83 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
-class GildedRose(object):
+class ItemUpdater:
+    def update(self, item):
+        self.update_quality(item)
+        self.update_sell_in(item)
+        if item.sell_in < 0:
+            self.update_expired_quality(item)
 
+    def update_quality(self, item):
+        self.decrease_quality(item, 1)
+
+    def update_sell_in(self, item):
+        item.sell_in -= 1
+
+    def update_expired_quality(self, item):
+        self.decrease_quality(item, 1)
+
+    def increase_quality(self, item, amount):
+        item.quality = min(50, item.quality + amount)
+
+    def decrease_quality(self, item, amount):
+        item.quality = max(0, item.quality - amount)
+
+
+class AgedBrieUpdater(ItemUpdater):
+    def update_quality(self, item):
+        self.increase_quality(item, 1)
+
+    def update_expired_quality(self, item):
+        self.increase_quality(item, 1)
+
+
+class BackstagePassUpdater(ItemUpdater):
+    def update_quality(self, item):
+        increase = 1
+        if item.sell_in < 11:
+            increase += 1
+        if item.sell_in < 6:
+            increase += 1
+        self.increase_quality(item, increase)
+
+    def update_expired_quality(self, item):
+        item.quality = 0
+
+
+class SulfurasUpdater(ItemUpdater):
+    def update(self, item):
+        return
+
+
+class ConjuredUpdater(ItemUpdater):
+    def update_quality(self, item):
+        self.decrease_quality(item, 2)
+
+    def update_expired_quality(self, item):
+        self.decrease_quality(item, 2)
+
+
+class UpdaterFactory:
+    _UPDATERS = {
+        "Aged Brie": AgedBrieUpdater(),
+        "Backstage passes to a TAFKAL80ETC concert": BackstagePassUpdater(),
+        "Sulfuras, Hand of Ragnaros": SulfurasUpdater(),
+    }
+
+    _CONJURED_PREFIX = "Conjured"
+
+    @classmethod
+    def for_item(cls, item):
+        if item.name.startswith(cls._CONJURED_PREFIX):
+            return ConjuredUpdater()
+        return cls._UPDATERS.get(item.name, ItemUpdater())
+
+
+class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
+            UpdaterFactory.for_item(item).update(item)
--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -5,11 +5,34 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+    def update_item(self, name, sell_in, quality):
+        item = Item(name, sell_in, quality)
+        GildedRose([item]).update_quality()
+        return item
+
+    def test_conjured_item_degrades_twice_as_fast_before_sell_date(self):
+        item = self.update_item("Conjured Mana Cake", 3, 6)
+
+        self.assertEqual(2, item.sell_in)
+        self.assertEqual(4, item.quality)
+
+    def test_conjured_item_degrades_four_times_after_sell_date(self):
+        item = self.update_item("Conjured Mana Cake", 0, 6)
+
+        self.assertEqual(-1, item.sell_in)
+        self.assertEqual(2, item.quality)
+
+    def test_conjured_item_quality_never_drops_below_zero_before_sell_date(self):
+        item = self.update_item("Conjured Mana Cake", 3, 1)
+
+        self.assertEqual(2, item.sell_in)
+        self.assertEqual(0, item.quality)
+
+    def test_conjured_item_quality_never_drops_below_zero_after_sell_date(self):
+        item = self.update_item("Conjured Mana Cake", 0, 3)
+
+        self.assertEqual(-1, item.sell_in)
+        self.assertEqual(0, item.quality)
 
         
 if __name__ == '__main__':
